47a1de8a4969386590cee096ed800b534f647e21
zeebraa00 <zeebraa00@gmail.com>
2020-07-22 21:29:09 +0900

    [add] next page routing

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const About = () => (
+    <div>
+        <h2>
+            안녕 내 이름은 정재헌이다
+        </h2>
+    </div>
+)
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import Link from "next/link"
+
 const Index = () => (
     <div>
         <h1>
             Hello, Next.js
         </h1>
+        <h2>
+            <Link href="/about">
+                <div style={{background : "black", color: "white"}} >소개</div>
+            </Link>
+        </h2>
     </div>
 );
 
