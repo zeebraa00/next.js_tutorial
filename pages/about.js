21007dc89e91289e9d35da41e95c446c3765d94e
zeebraa00 <zeebraa00@gmail.com>
2020-07-22 23:35:31 +0900

    [edit] jsx 스타일링

--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,14 @@ import Layout from "../components/Layout";
 
 const About = () => (
     <Layout>
-        <h2>
-            안녕 내 이름은 정재헌이다
-        </h2>
+        <style jsx>
+            {`
+                h2 {
+                    color : green 
+                }
+            `}
+        </style>
+        <h2>안녕 내 이름은 정재헌이다</h2>
     </Layout>
 )
 
