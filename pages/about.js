e5bc842ceba673ffdab3777964e7c812a72e6f1b
zeebraa00 <zeebraa00@gmail.com>
2020-07-22 21:48:30 +0900

    [add] Header 추가(공용컴포넌트)

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,8 @@
+import Header from "../components/Header";
+
 const About = () => (
     <div>
+        <Header />
         <h2>
             안녕 내 이름은 정재헌이다
         </h2>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
-import Link from "next/link"
+import Link from "next/link";
+import Header from "../components/Header";
 
 const Index = () => (
     <div>
+        <Header />
         <h1>
             Hello, Next.js
         </h1>
         <h2>
             <Link href="/about">
-                <div style={{background : "black", color: "white"}} >소개</div>
+                <div>
+                    <a style={{background : "black", color: "white"}}> 소개 </a>
+                </div>
             </Link>
         </h2>
     </div>
