97eb8520a13f1fd51a40e620c839584b821557b4
zeebraa00 <zeebraa00@gmail.com>
2020-07-22 23:12:05 +0900

    [add] axios 이용 ajax 통신

--- pages/ssr-test.js
+++ pages/ssr-test.js
@@ -1,16 +1,26 @@
 import Layout from "../components/Layout";
+import axios from "axios";
 
 class SSRTest extends React.Component {
     static async getInitialProps ({req}) {
-        return req
-        ? {from : 'server'} // 서버에서 실행할 시
-        : {from : 'client'} // 클라이언트에서 실행할 시
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        return {
+            users : response.data
+        }
     };
 
     render() {
+        const {users} = this.props;
+
+        const userList = users.map(
+            user => <li key={user.id}>{user.username}</li>
+        )
+
         return (
             <Layout>
-                {this.props.from} 에서 실행됨 ㅋ
+                <ul>
+                    {userList}
+                </ul>
             </Layout>
         )
     }
